4e9ecc60d097d8b33a20dcfa2a81b4ea6c37e5ab
srawat-gd <srawat@griddynamics.com>
2024-05-24 16:18:19 +0530

    Initial Commit

---
+++ TestDemo/TestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDemoApp.swift
+//  TestDemo
+//
+//  Created by Shivam Rawat on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
